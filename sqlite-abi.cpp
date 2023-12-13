954f97c369bc81f4937ed47f359afab0cb424935
Cyberhan123 <255542417@qq.com>
2023-12-13 14:59:32 +0800

    add new abi api

--- sqlite-abi.cpp
+++ sqlite-abi.cpp
@@ -2,18 +2,18 @@
 #include "json.hpp"
 #include <string>
 
-sqlite3* sqlite3_abi_init(const char* path) {
-    sqlite3* db;
+sqlite3 *sqlite3_abi_init(const char *path) {
+    sqlite3 *db;
     const auto result = sqlite3_open(path, &db);
-    if (result != SQLITE_OK) {
+    if (result != SQLITE_OK && db == nullptr) {
         return nullptr;
     }
     return db;
 }
 
-const char* sqlite3_abi_exec(sqlite3* db, const char* sql) {
+const char *sqlite3_abi_exec(sqlite3 *db, const char *sql) {
     nlohmann::json resultArray = nlohmann::json::array();
-    auto state = sqlite3_exec(db, sql, [](void* data, int argc, char** argv, char** colName) -> int {
+    auto state = sqlite3_exec(db, sql, [](void *data, int argc, char **argv, char **colName) -> int {
         auto row = nlohmann::json::object();
         for (int i = 0; i < argc; i++) {
             row[colName[i]] = argv[i] ? argv[i] : "NULL";
@@ -24,24 +24,22 @@ const char* sqlite3_abi_exec(sqlite3* db, const char* sql) {
     }, nullptr, nullptr);
 
     if (state != SQLITE_OK) {
-        nlohmann::json errorJson = {
-            {"code", SQLITE_ERROR},
-            {"message", "Error executing query: " + std::string(sqlite3_errmsg(db))},
-            {"list", nlohmann::json::array()}
-        };
-        const auto errorStr = errorJson.dump();
-        const auto resultStr = new char[errorStr.size() + 1];
-        std::strcpy(resultStr, errorStr.c_str());
-        return resultStr;
+        return "";
     }
-
-    nlohmann::json successJson = {
-        {"code", SQLITE_OK},
-        {"message", "Query executed successfully"},
-        {"list", resultArray}
-    };
-    const std::string successStr = successJson.dump();
+    const std::string successStr = resultArray.dump();
     const auto resultStr = new char[successStr.size() + 1];
-    std::strcpy(resultStr, successStr.c_str());
+    std::memcpy(resultStr, successStr.c_str(), successStr.size());
     return resultStr;
+}
+
+int sqlite3_abi_close(sqlite3 *db) {
+    return sqlite3_close(db);
+}
+
+const char *sqlite3_abi_errmsg(sqlite3 *db) {
+    return sqlite3_errmsg(db);
+}
+
+int sqlite3_abi_errcode(sqlite3 *db) {
+    return sqlite3_errcode(db);
 }
